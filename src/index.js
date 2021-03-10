f9926927f56242c0f2c516e23d7afdec08bdbdfd
zhangh <506365426@qq.com>
2021-03-10 19:28:23 +0800

    style:  prettierrc 排版

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@
 export default function () {
   console.log(foo);
 } */
-import { version } from "../package.json";
+import { version } from '../package.json';
 
 export default function () {
-  const hello = "hello world";
-  const aFruitList = ["apple", "banana", "pear", "pineapple"];
+  const hello = 'hello world';
+  const aFruitList = ['apple', 'banana', 'pear', 'pineapple'];
   for (let [index, elem] of aFruitList.entries()) {
     console.log(index, elem);
   }
   const p1 = new Promise(function (resolve, reject) {
     setTimeout(() => {
-      resolve("hello-promise");
+      resolve('hello-promise');
     }, 3000);
   });
   p1.then((result) => console.warn(result));
-  console.log("version " + version + hello);
+  console.log('version ' + version + hello);
 }
